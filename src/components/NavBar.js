88e93a2b449bf01dbd5f5a8d356cd3a1bd37b1f9
Burcu Bayraö <burju7@yahoo.com>
2023-06-06 12:50:56 +0300

    completed assigment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,13 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const navLinks = links.map((link, index) => (
+    <a key={index} href={link}>
+      {link}
+    </a>
+  ));
+
+  return <nav>{navLinks}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  console.log(technologies);
+  const technologySpan = technologies.map((technology, index) => {
+    return <span key={index}>{technology}</span>;
+  });
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
+        {technologySpan}
         {/* render a <span> for each technology in the technologies array */}
       </div>
     </div>
